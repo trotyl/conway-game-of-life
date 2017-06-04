dd5ad36cbdd1e7d792a827bf8ea07cba88008316
Trotyl <trotyl@qq.com>
2017-06-04 20:52:01 +0800

    introduce neighbor counting algorism

--- src/app/core/neighbor-counter.service.spec.ts
+++ src/app/core/neighbor-counter.service.spec.ts
@@ -2,6 +2,34 @@ import { TestBed, inject } from '@angular/core/testing'
 
 import { NeighborCounterService } from './neighbor-counter.service'
 
+interface Point {
+  x: number
+  y: number
+  c: string
+}
+
+function parseMockup(mockup: string): Point[] {
+  return mockup.split(/\s/g)
+    .filter(row => row.length !== 0)
+    .map((row, i) => ({ x: i, chars: row.split('').map((c, j) => ({ c: c, y: j }))}))
+    .reduce(
+      (res, { x, chars }) => [...res, ...chars.map(({ c, y }) => ({ x, y, c }))],
+      [] as Point[])
+}
+
+function parsePositions(mockup: string): Set<string> {
+  const positions = parseMockup(mockup)
+    .filter(({ c }) => c !== '-')
+    .map(({ x, y }) => `${x},${y}`)
+  return new Set(positions)
+}
+
+function parseCounts(mockup: string): [string, number][] {
+  return parseMockup(mockup)
+    .filter(({ c }) => c !== '0')
+    .map(({ x, y, c }) => [`${x},${y}`, Number.parseInt(c)] as [string, number])
+}
+
 describe('NeighborCounterService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -11,5 +39,21 @@ describe('NeighborCounterService', () => {
 
   it('should be created', inject([NeighborCounterService], (service: NeighborCounterService) => {
     expect(service).toBeTruthy()
+  }))
+
+  it('should be able to count neighbors', inject([NeighborCounterService], (service: NeighborCounterService) => {
+    const counts = service.calculate(parsePositions(`
+      ----
+      -XX-
+      -X--
+      ----
+    `))
+
+    expect(Array.from(counts.entries()).sort()).toEqual(parseCounts(`
+      1221
+      2221
+      2231
+      1110
+    `).sort())
   }))
 })
--- src/app/core/neighbor-counter.service.ts
+++ src/app/core/neighbor-counter.service.ts
@@ -2,5 +2,31 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class NeighborCounterService {
-  calculate(cells: Set<string>): any { }
+  calculate(cells: Set<string>): Map<string, number> {
+    const counts = new Map<string, number>()
+
+    cells.forEach((cell) => {
+      const [x, y] = cell.split(',').map(str => Number.parseInt(str)) as [number, number]
+
+      const neighbors = [
+        `${x - 1},${y - 1}`,
+        `${x - 1},${y}`,
+        `${x - 1},${y + 1}`,
+        `${x},${y - 1}`,
+        `${x},${y + 1}`,
+        `${x + 1},${y - 1}`,
+        `${x + 1},${y}`,
+        `${x + 1},${y + 1}`,
+      ]
+
+      neighbors.forEach(neighbor => {
+        if (!counts.has(neighbor)) {
+          counts.set(neighbor, 0)
+        }
+        counts.set(neighbor, counts.get(neighbor) + 1)
+      })
+    })
+
+    return counts
+  }
 }
