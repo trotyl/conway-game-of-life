1cd037317ffaf12924f454f0e903f35b0d7ce847
Trotyl <trotyl@qq.com>
2017-06-04 11:52:34 +0800

    use short path in module import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing'
 
 import { AppComponent } from './app.component'
-import { ContainerComponent } from './playground/container/container.component'
+import { ContainerComponent } from './playground'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { ContainerComponent } from './playground/container/container.component'
+import { ContainerComponent } from './playground'
 
 @NgModule({
   declarations: [
